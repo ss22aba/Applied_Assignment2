6024688fa13096d8711173c0a6da020242878b83
ss22aba <117084208+ss22aba@users.noreply.github.com>
2022-12-08 00:21:50 +0530

    Bar graghs added

--- assignment2.py
+++ assignment2.py
@@ -5,6 +5,8 @@ Created on Wed Dec  7 18:20:49 2022
 
 @author: asus
 """
+
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -34,7 +36,8 @@ def get_data_frames(filename):
     df2 = df2.pivot_table('value',['Years','Indicator Name','Indicator Code'],'Country Name').reset_index()
 
     return df, df2
-
+    df.dropna()
+    df2.dropna()
 
 countries = ['Japan','Germany','Canada','United Kingdom']
 
@@ -51,17 +54,89 @@ plt.ylabel("Renewable energy consumption ")
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
-#-----------------------------------------
-# Line Plot 2
+# #-----------------------------------------
+# # Line Plot 2
 data1, data2 = get_data_frames('API_19_DS2_en_csv_v2_4700503.csv')
 data2 = data2.loc[data2['Indicator Code'].eq('SP.POP.TOTL')]
 
 plt.figure(figsize=(7,7))
 data2['Years'] = pd.to_numeric(data2['Years'])
-data2.plot("Years", countries, title='Total population')
+data2.plot("Years", countries, title='Total population in %')
 plt.xlabel("Years")
-plt.ylabel("Total population")
+plt.ylabel("Total population in %")
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
+#------------------------------------------------------------------------------
+#Bar chart1
 
+'''This function plot bar chart from the dataset using dataframe.
+Bar chart shows duration of sunlight in UK between 2000-2005 for all seasons.
+'''
+df_bar, df2_bar = get_data_frames('API_19_DS2_en_csv_v2_4700503.csv')
+print(df2_bar)
+df2_bar = df2_bar.loc[df2_bar['Indicator Code'].eq('AG.LND.AGRI.ZS')]
+# loc function used to get data of specific years 2000-2005 from dataframe.
+print(df2_bar)
+df2_bar = df2_bar.loc[df2_bar['Years'].isin(['2000','2001','2002','2003','2004','2005'])]
+
+print(df2_bar)
+
+x = np.arange(6)
+width = 0.2
+years = df2_bar['Years'].tolist()
+
+plt.figure(dpi=144)
+#To show main title of plot.
+plt.title('Agricultural land (sq. km)')
+# x is used for grouped bar chart. x is the position of 1 bar.
+# x+0.2, x-0.2, x-0.4 these postion are used to show bar charts in groups.
+plt.bar(x, df2_bar['Japan'], width, label='Japan')
+plt.bar(x+0.2, df2_bar['Germany'], width, label='Germany')
+plt.bar(x-0.2, df2_bar['Canada'], width, label='Canada')
+plt.bar(x+0.4, df2_bar['United Kingdom'], width, label='United Kingdom')
+# xticks are used to show years on x-axis of the plot.
+plt.xticks(x, years)
+# plt.yticks(np.arange(100,1000,200))
+# x-axis label
+plt.xlabel('Years')
+# y-axis label
+plt.ylabel('Agricultural land (% of land area)')
+# legend function is called to make labels visibles on the chart like Seanson names.
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()
+
+#-----------------------------------------------------------------------
+#Bar chart 2
+data_bar, data2_bar = get_data_frames('API_19_DS2_en_csv_v2_4700503.csv')
+print(df2_bar)
+data2_bar = data2_bar.loc[data2_bar['Indicator Code'].eq('AG.LND.FRST.ZS')]
+# loc function used to get data of specific years 2000-2005 from dataframe.
+print(data2_bar)
+data2_bar = data2_bar.loc[data2_bar['Years'].isin(['2000','2001','2002','2003','2004','2005'])]
+
+print(data2_bar)
+
+x = np.arange(6)
+width = 0.2
+years = data2_bar['Years'].tolist()
+
+plt.figure(dpi=144)
+#To show main title of plot.
+plt.title('Forest area (% of land area)')
+# x is used for grouped bar chart. x is the position of 1 bar.
+# x+0.2, x-0.2, x-0.4 these postion are used to show bar charts in groups.
+plt.bar(x, data2_bar['Japan'], width, label='Japan')
+plt.bar(x+0.2, data2_bar['Germany'], width, label='Germany')
+plt.bar(x-0.2, data2_bar['Canada'], width, label='Canada')
+plt.bar(x+0.4, data2_bar['United Kingdom'], width, label='United Kingdom')
+# xticks are used to show years on x-axis of the plot.
+plt.xticks(x, years)
+# plt.yticks(np.arange(100,1000,200))
+# x-axis label
+plt.xlabel('Years')
+# y-axis label
+plt.ylabel('Forest area (% of land area)')
+# legend function is called to make labels visibles on the chart like Seanson names.
+plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
+plt.show()

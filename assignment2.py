6b561f65b612fa92b079d791d52e86e0ce37680e
ss22aba <117084208+ss22aba@users.noreply.github.com>
2022-12-08 21:43:36 +0530

    2 lineplots, 2 pieplots, 2 barplots

--- assignment2.py
+++ assignment2.py
@@ -73,8 +73,8 @@ plt.figure(figsize=(7,7))
 df2['Years'] = pd.to_numeric(df2['Years'])
 
 #Plots the line gragh also  the title is set.
-df2.plot("Years", countries, 
-         title='Renewable energy consumption(% of total final energy consumption)')
+df2.plot("Years", countries, title=
+         'Renewable energy consumption(% of total final energy consumption)')
 
 # Setting the x label as Years.
 plt.xlabel("Years")
@@ -118,17 +118,17 @@ plt.savefig("Population lie plot")
 plt.show()
 
 #------------------------------------------------------------------------------
-#Bar chart1
+#Plots the bar chart showing the Agricultural land (% of land area) for the
+#countries : UK, Canada, Germany,Japan.
 
-'''This function plot bar chart from the dataset using dataframe.
-Bar chart shows duration of sunlight in UK between 2000-2005 for all seasons.
-'''
 df_bar, df2_bar = get_data_frames('API_19_DS2_en_csv_v2_4700503.csv')
 print(df2_bar)
 df2_bar = df2_bar.loc[df2_bar['Indicator Code'].eq('AG.LND.AGRI.ZS')]
-# loc function used to get data of specific years 2000-2005 from dataframe.
 print(df2_bar)
-df2_bar = df2_bar.loc[df2_bar['Years'].isin(['2000','2001','2002','2003','2004','2005'])]
+
+# loc function used to get data of specific years 2000-2005 from dataframe.
+df2_bar = df2_bar.loc[df2_bar['Years'].isin(['2000','2001','2002',
+                                             '2003','2004','2005'])]
 
 print(df2_bar)
 
@@ -137,32 +137,41 @@ width = 0.2
 years = df2_bar['Years'].tolist()
 
 plt.figure(dpi=144)
+
 #To show main title of plot.
 plt.title('Agricultural land (% of land area)')
+
 # x is used for grouped bar chart. x is the position of 1 bar.
 # x+0.2, x-0.2, x-0.4 these postion are used to show bar charts in groups.
 plt.bar(x, df2_bar['Japan'], width, label='Japan')
 plt.bar(x+0.2, df2_bar['Germany'], width, label='Germany')
 plt.bar(x-0.2, df2_bar['Canada'], width, label='Canada')
 plt.bar(x+0.4, df2_bar['United Kingdom'], width, label='United Kingdom')
+
 # xticks are used to show years on x-axis of the plot.
 plt.xticks(x, years)
+
 # plt.yticks(np.arange(100,1000,200))
 # x-axis label
 plt.xlabel('Years')
+
 # y-axis label
 plt.ylabel('Agricultural land (% of land area)')
-# legend function is called to make labels visibles on the chart like Seanson names.
+
+# legend function is called to make labels visibles on the chart 
 plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 plt.show()
 
 #-----------------------------------------------------------------------
-#Bar chart 2
+#Plots the bar chart showing the Forest area(% of land area) for the countries:
+# Japan, UK, Germany, Canada.
+
 data_bar, data2_bar = get_data_frames('API_19_DS2_en_csv_v2_4700503.csv')
 print(df2_bar)
 data2_bar = data2_bar.loc[data2_bar['Indicator Code'].eq('AG.LND.FRST.ZS')]
-# loc function used to get data of specific years 2000-2005 from dataframe.
 print(data2_bar)
+
+# loc function used to get data of specific years 2000-2005 from dataframe.
 data2_bar = data2_bar.loc[data2_bar['Years'].
                           isin(['2000','2001','2002','2003','2004','2005'])]
 
@@ -172,7 +181,7 @@ x = np.arange(6)
 width = 0.2
 years = data2_bar['Years'].tolist()
 
-#P
+#Ploting the figure
 plt.figure(dpi=144)
 
 #To show main title of plot.
@@ -236,6 +245,46 @@ plt.title("Population growth (annual %)")
 
 #Save the figure
 plt.savefig("populationgrowth_pie")
+
+#Show the figure
+plt.show()
+
+#------------------------------------------------------------------------------
+# Plotting the  pie chart of Arable land (% of land area) for the countries :
+# Japan, UK, Canada and Germany
+
+#Read the file into dataframes.
+df, df2 = get_data_frames('API_19_DS2_en_csv_v2_4700503.csv')
+df2 = df2.loc[df2['Indicator Code'].eq('AG.LND.ARBL.ZS')]
+
+#removing all the nan values.
+df2.dropna()
+print(df2.info())
+
+Japan= np.sum(df2['Japan'])
+Canada= np.sum(df2['Canada'])
+
+Germany=np.sum(df2['Germany'])
+uk= np.sum(df2["United Kingdom"])
+
+total= Japan +Canada+uk+Germany
+
+Japan= Japan/ total*100
+Canada= Canada/ total*100
+Germany=Germany/ total*100
+uk= uk/ total*100
+
+arable_land= np.array([Japan,Canada,uk,Germany])
+
+#Plot the figure
+plt.figure(dpi=144)
+plt.pie(arable_land, labels= countries,autopct=('%1.1f%%'))
+
+#Set the title
+plt.title("Arable land (% of land area)")
+
+#Save the figure
+plt.savefig("Arableland_pie")
 
 #Show the figure
 plt.show()
